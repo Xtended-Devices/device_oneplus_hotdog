610e122206b55f2aacd9a08d0a824e9cbe429890
Mukesh Singh <mks22584@gmail.com>
2021-10-04 18:44:31 +0530

    hotdog: merge overlay-xtended into overlay
    - keep at one place

--- device.mk
+++ device.mk
@@ -11,8 +11,7 @@ $(call inherit-product, vendor/oneplus/hotdog/hotdog-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-xtended
+    $(LOCAL_PATH)/overlay
 
 # Audio
 PRODUCT_COPY_FILES += \
